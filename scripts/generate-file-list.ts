ebf03502560c2d026cfe5af9543a47371dbaf5ee
Steve Kinney <hello@stevekinney.net>
2023-10-30 06:42:12 -0600

    Update content scripts

--- scripts/generate-file-list.ts
+++ scripts/generate-file-list.ts
@@ -8,7 +8,7 @@ try {
 	const fileNames: string[] = [];
 
 	for (const file of files) {
-		if ((await stat(path.join(directoryPath, file))).isFile()) {
+		if ((await stat(path.join(directoryPath, file))).isFile() && file.endsWith('.md')) {
 			fileNames.push(file);
 		}
 	}
